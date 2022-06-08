5b850ad6e6266875702d3841e9d411a4d5569758
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-08 23:16:33 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const chai = "chai";
+function receivesAFunction(callback){
+    const spy = chai;
+
+    return callback (spy);   
+}
+receivesAFunction(function(){return spy()});
+
+function returnsANamedFunction()  {
+    
+    return function fn(){
+        return fn=returnsANamedFunction();
+    }
+   
+}
+returnsANamedFunction()
+
+ function returnsAnAnonymousFunction(){
+     return function (){}
+ }
